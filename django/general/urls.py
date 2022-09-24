c2732e4e681d6df0599b9fbb18210ca4d01a0a15
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-09-24 18:15:02 +0100

    set site style

--- django/general/urls.py
+++ django/general/urls.py
@@ -5,8 +5,10 @@ app_name = 'general'
 
 urlpatterns = [
     path('', views.WelcomeTemplateView.as_view(), name='welcome'),
+
     path('about/', views.AboutTemplateView.as_view(), name='about'),
-    path('accessibility/', views.AccessibilityTemplateView.as_view(), name='accessibility'),
-    path('cookies/', views.CookiesTemplateView.as_view(), name='cookies'),
+    path('about/accessibility/', views.AccessibilityTemplateView.as_view(), name='accessibility'),
+    path('about/cookies/', views.CookiesTemplateView.as_view(), name='cookies'),
+
     path('robots.txt', views.RobotsTemplateView.as_view(), name='robots')
 ]
