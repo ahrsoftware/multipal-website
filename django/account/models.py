d71ee68b2f2d15fc74bbdabf37fbb5a53004121d
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2022-10-25 16:20:06 +0100

    custom user manager for case insensitive username

--- django/account/models.py
+++ django/account/models.py
@@ -1,10 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        """
+        Allow users to login with case-insensitive username
+
+        E.g. both "My.Name@uni.ac.uk" and "my.name@uni.ac.uk" will allow users to login
+        """
+        return self.get(username__iexact=username)
 
 
 class User(AbstractUser):
     """
     Custom user extends the standard Django user model, providing additional properties
     """
+
+    objects = CustomUserManager()  # Custom user manager used to allow for case-insensitive usernames
 
     @property
     def name(self):
@@ -22,6 +34,8 @@ class User(AbstractUser):
         return self.name
 
     def save(self, *args, **kwargs):
-        # Username to be same as email
+        # Force email and username to be lower case and identical, so users can login with email
+        self.email = self.email.strip().lower()
         self.username = self.email
+
         super().save(*args, **kwargs)
